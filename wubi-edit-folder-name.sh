fe9322e1565b5a1d2eca56289d988d5847216879
Jason Mun <30671857+yomun@users.noreply.github.com>
2020-07-19 16:25:18 +0800

    Update wubi-edit-folder-name.sh

--- wubi-edit-folder-name.sh
+++ wubi-edit-folder-name.sh
@@ -36,30 +36,28 @@ cp -r build/winboot /host
 
 # --------------------- Windows 7 (修改 Windows Boot Manager 的设置和替换启动文件) ---------------------
 
-# 1) 先将 C:\ubuntu 改名为 C:\ubuntu_1710
-
-# 2) 用软件 EasyBCD 2.3　查看现在 ubuntu 17.10 所用的 ID
-# identifier              {905b6225-f81c-11e5-8d07-e79e7f21200d}
-# device                  partition=C:
-# path                    \ubuntu\winboot\wubildr.mbr
-# description             Ubuntu 17.10
-
-# 3) 然后进入 command prompt 输入以下, 修改以上的 path
-# > bcdedit /set {905b6225-f81c-11e5-8d07-e79e7f21200d} path \ubuntu_1710\winboot\wubildr.mbr
-
-# 4) 将以上制作 C:\winboot　里的 5 个文件, 取代现在原有的 C:\ubuntu_1710\winboot
-# 也要将 C:\wubildr 和 C:\wubildr.mbr 更换掉 (是否能进入 Grub 2, 这可是关键..)
-
-# --------------------- Ubuntu 17.10 (进入系统修改设置) ---------------------
-
 # 1) 进入 Grub 2, 选 ubuntu, 按 e, 进入编辑模式
-# 将 2 处路径 /ubuntu/disks/root.disk 改为 /ubuntu_1710/disks/root.disk
-# 然后按 ctrl+x 运行
 
-# 2) 进入 ubuntu 后, 将 /etc/fstab 修改下 root.disk 与 swap.disk 的路径
-# $ sudo gedit /etc/fstab
-# /ubuntu/ 改为 /ubuntu_1710/
+# 将 2 处路径 /ubuntu/disks/root.disk 
+# 改为 
+# /ubuntu_16/disks/root.disk
+
+# 将 ro quiet splash $vt_handoff
+# 改为
+# rw single init=/bin/bash
+
+# 然后按 Ctrl+x 运行, 进入单机模式 bash
+
+# 2) 进入 bash 后, 将 /etc/fstab 修改下 root.disk 与 swap.disk 的路径
+
+# $ root@none:/# vi /etc/fstab
+
+# /ubuntu/ 改为 /ubuntu_16/
 
 # 3) 更新 Grub 2
-# $ sudo update-grub
 
+# $ root@none:/# update-grub
+
+# 4) 运行, 进入 GNOME
+
+# root@none:/# exec /sbin/init
